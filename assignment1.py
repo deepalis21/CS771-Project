e54f8dda8f421e31858bf67a640371cef51c37d9
Deepali Singh <133847806+deepalis21@users.noreply.github.com>
2025-03-22 11:47:13 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,69 +1,59 @@
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+import sklearn
+from sklearn.linear_model import LogisticRegression
+from scipy.linalg import khatri_rao
 
-# Define any new functions, variables, classes here
-# For example, classes to create the Tree, Nodes etc
-class BigramWordPredictor:
-    def __init__(self):
-        self.model = DecisionTreeClassifier()
-        self.vocab = {}
+# You are allowed to import any submodules of sklearn that learn linear models e.g. sklearn.svm etc
+# You are not allowed to use other libraries such as keras, tensorflow etc
+# You are not allowed to use any scipy routine other than khatri_rao
 
-    def preprocess_word(self, word):
-        bigrams = [word[i:i+2] for i in range(len(word)-1)]
-        return bigrams
+# SUBMIT YOUR CODE AS A SINGLE PYTHON (.PY) FILE INSIDE A ZIP ARCHIVE
+# THE NAME OF THE PYTHON FILE MUST BE submit.py
 
-    def fit_vectorizer(self, bigram_words):
-        bigram_set = set()
-        for bigrams in bigram_words:
-            bigram_set.update(bigrams)
-        self.vocab = {bigram: i for i, bigram in enumerate(bigram_set)}
+# DO NOT CHANGE THE NAME OF THE METHODS my_fit, my_map etc BELOW
+# THESE WILL BE INVOKED BY THE EVALUATION SCRIPT. CHANGING THESE NAMES WILL CAUSE EVALUATION FAILURE
 
-    def transform(self, bigram_words):
-        X = np.zeros((len(bigram_words), len(self.vocab)))
-        for i, bigrams in enumerate(bigram_words):
-            for bigram in bigrams:
-                if bigram in self.vocab:
-                    X[i, self.vocab[bigram]] += 1
-        return X
-
-    def my_fit(self, dictionary):
-        bigram_words = [self.preprocess_word(word) for word in dictionary]
-        self.fit_vectorizer(bigram_words)
-        X = self.transform(bigram_words)
-        y = np.array(dictionary)
-        self.model.fit(X, y)
-
-    def my_predict(self, bigrams):
-        X = self.transform([bigrams])
-        predictions = self.model.predict_proba(X)
-        top_indices = np.argsort(predictions[0])[-5:][::-1]
-        top_words = [self.model.classes_[i] for i in top_indices]
-        return top_words
+# You may define any new functions, variables, classes here
+# For example, functions to calculate next coordinate or step length
 
 ################################
 # Non Editable Region Starting #
 ################################
-def my_fit(words):
+
+
+def my_fit(X_train, y0_train, y1_train):
+    # Transform the challenges
+    transformed_challenges = my_map(X_train)
+
+    # Train two linear models
+    model0 = LogisticRegression(C=10.0, tol=1e-5, solver='liblinear')
+    model0.fit(transformed_challenges, y0_train)
+
+    model1 = LogisticRegression(C=10.0, tol=1e-5, solver='liblinear')
+    model1.fit(transformed_challenges, y1_train)
+
+    # Extract weights and biases
+    W0 = model0.coef_[0]
+    b0 = model0.intercept_[0]
+    W1 = model1.coef_[0]
+    b1 = model1.intercept_[0]
+
+    return W0, b0, W1, b1
 ################################
 #  Non Editable Region Ending  #
 ################################
-
-    # Do not perform any file IO in your code
-    # Use this method to train your model using the word list provided
-    bigram_predictor = BigramWordPredictor()
-    bigram_predictor.my_fit(words)
-    return bigram_predictor  # Return the trained model
 
 ################################
 # Non Editable Region Starting #
 ################################
-def my_predict(model, bigram_list):
+
+
+def my_map(X):
+    # Using the Khatri-Rao product to transform the feature vector
+    transformed_features = khatri_rao(X.T, X.T).T
+    return transformed_features
 ################################
 #  Non Editable Region Ending  #
 ################################
 
-    # Do not perform any file IO in your code
-    # Use this method to predict on a test bigram_list
-    # Ensure that you return a list even if making a single guess
-    guess_list = model.my_predict(bigram_list)
-    return guess_list
+# Ensure the file has the correct structure and logic.
